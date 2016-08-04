91998b2a01596dc3bacecc02fdd8b132825348b9
Jae-Huh <jaeeunhuh@gmail.com>
2016-08-04 22:10:15 +1200

    Add tracking of bowls

--- js/main.js
+++ js/main.js
@@ -3,14 +3,15 @@ $(document).ready(function(){
   $('#bowl').click(function(){
     var score = Math.floor(Math.random() * 11);
     changeScore(score);
-
+    numberOfBowls = numberOfBowls + 1
     totalscore = totalscore + score;
 
     addScore(totalscore);
+    console.log(numberOfBowls)
   });
 
 });
-
+var numberOfBowls = 0;
 var totalscore = 0;
 
 var changeScore = function(text){
